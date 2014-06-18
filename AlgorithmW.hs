679e052cb57f11f9d4dd4b9be8da50feae09f390
Eyal Lotem <eyal.lotem@elastifile.com>
2014-06-18 16:30:11 +0300

    lhs -> hs

--- AlgorithmW.hs
+++ AlgorithmW.hs
@@ -1,75 +1,9 @@
-\documentclass[a4paper,11pt]{article}
-
-\usepackage[margin=2.5cm]{geometry}
-\usepackage{hyperref}
-
-%include polycode.fmt
-%format alpha = "\alpha"
-%format Set.empty = "\emptyset"
-%format `Set.union` = "\cup"
-%format `Set.difference` = "~\backslash~"
-%format Set.singleton n = "\{" n "\}"
-%format <+> = "\left<+\right>"
-
-\title{\bf Algorithm W Step by Step}
-\author{Martin Grabm{\"u}ller}
-\date{}
-
-\begin{document}
-\maketitle
-
-\begin{abstract}\noindent
-In this paper we develop a complete implementation of the classic
-algorithm W for Hindley-Milner polymorphic type inference in Haskell.
-\end{abstract}
-
-\section{Introduction}
-
-Type inference is a tricky business, and it is even harder to learn
-the basics, because most publications are about very advanced topics
-like rank-N polymorphism, predicative/impredicative type systems,
-universal and existential types and so on.  Since I learn best by
-actually developing the solution to a problem, I decided to write a
-basic tutorial on type inference, implementing one of the most basic
-type inference algorithms which has nevertheless practical uses as the
-basis of the type checkers of languages like ML or Haskell.
-
-The type inference algorithm studied here is the classic Algoritm W
-proposed by Milner \cite{Milner1978Theory}.  For a very readable
-presentation of this algorithm and possible variations and extensions
-read also \cite{Heeren2002GeneralizingHM}.  Several aspects of this
-tutorial are also inspired by \cite{Jones1999THiH}.
-\footnote{Copied from
-  \url{http://www.grabmueller.de/martin/www/pub/AlgorithmW.en.html} and edited by
-  Wei Hu.  Unfortunately the bibliography is missing.
-}
-\footnote{The most helpful references are
-  \url{http://www.cs.uu.nl/research/techreps/repo/CS-2002/2002-031.pdf}
-\emph{Generalizing Hindley-Milner Type Inference Algorithms}, and
-Chapter 22 of \emph{TAPL}.}
-\footnote{A Cornell course touchs on this topic and gives an OCaml implementation.
-            \url{http://www.cs.cornell.edu/Courses/cs3110/2009fa/lectures/lec26a.htm}}
-
-\section{Algorithm W}
-
-We start with the necessary imports.  For representing environments
-(also called contexts in the literature) and substitutions, we import
-module |Data.Map|.  Sets of type variables etc. will be represented as
-sets from module |Data.Set|.
-
-\begin{code}
 {-# LANGUAGE StandaloneDeriving, DeriveFunctor, DeriveFoldable, DeriveTraversable, DeriveGeneric #-}
 module AlgorithmW where
 
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-\end{code}
-
-Since we will also make use of various monad transformers, several
-modules from the monad template library are imported as well.
-\begin{code}
 import Control.Applicative
 import Control.DeepSeq
+import Control.DeepSeq.Generics (genericRnf)
 import Control.Lens
 import Control.Monad.Error
 import Control.Monad.Reader
@@ -79,25 +13,11 @@ import Control.Monad.Writer hiding ((<>))
 import Data.Foldable (Foldable)
 import Data.List
 import GHC.Generics
-import Control.DeepSeq.Generics (genericRnf)
-\end{code}
-
-The module |Text.PrettyPrint| provides data types and functions for
-nicely formatted and indented output.
-\begin{code}
-import qualified Text.PrettyPrint as PP
 import Text.PrettyPrint ((<+>), (<>))
-\end{code}
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Text.PrettyPrint as PP
 
-
-\subsection{Preliminaries}
-
-We start by defining the abstract syntax for both \emph{expressions}
-(of type |Exp|), \emph{types} (|Type|) and \emph{type schemes}
-(|Scheme|).  A type scheme $\forall a_1,...,a_n.t$ is a type in which a number of
-polymorphic type variables are bound to a universal quantifier.
-
-\begin{code}
 data Body exp =  EVar String
               |  ELit Lit
               |  EApp exp exp
@@ -133,26 +53,11 @@ data Type    =  TVar String
 instance NFData Type where rnf = genericRnf
 
 data Scheme  =  Scheme [String] Type
-\end{code}
-%
-In order to provide readable output and error messages, we define
-several pretty-printing functions for the abstract syntax.  These are
-shown in Appendix~\ref{sec:pretty-printing}.
 
-We will need to determine the free type variables of a type.  Function
-|ftv| implements this operation, which we implement in the type class
-|Types| because it will also be needed for type environments (to be
-defined below).  Another useful operation on types, type schemes and
-the like is that of applying a substitution.  A substitution only
-replaces free type variables, so the quantified type variables in a
-type scheme are not affected by a substitution.
-\begin{code}
 class Types a where
     ftv    ::  a -> Set.Set String
     apply  ::  Subst -> a -> a
-\end{code}
 
-\begin{code}
 instance Types Type where
     ftv (TVar n)      =  Set.singleton n
     ftv (TCon _)      =  Set.empty
@@ -170,28 +75,15 @@ instance Types Type where
     apply _s TRecEmpty = TRecEmpty
     apply s (TRecExtend name typ rest) =
       TRecExtend name (apply s typ) $ apply s rest
-\end{code}
-
-\begin{code}
 instance Types Scheme where
     ftv (Scheme vars t)      =  (ftv t) `Set.difference` (Set.fromList vars)
 
     apply s (Scheme vars t)  =  Scheme vars (apply (foldr substDelete s vars) t)
-\end{code}
-
-It will occasionally be useful to extend the |Types| methods to lists.
-\begin{code}
 instance Types a => Types [a] where
     apply s  =  map (apply s)
     ftv l    =  foldr Set.union Set.empty (map ftv l)
-\end{code}
-%
-Now we define substitutions, which are finite mappings from type
-variables to types.
-%
-\begin{code}
-newtype Subst = Subst (Map.Map String Type)
 
+newtype Subst = Subst (Map.Map String Type)
 instance Monoid Subst where
   mempty = Subst Map.empty
   mappend (Subst s1) (Subst s2) = Subst (s2 `Map.union` (Map.map (apply (Subst s2)) s1))
@@ -201,41 +93,16 @@ substLookup name (Subst s) = Map.lookup name s
 
 substDelete :: String -> Subst -> Subst
 substDelete name (Subst s) = Subst (Map.delete name s)
-\end{code}
-%
-Type environments, called $\Gamma$ in the text, are mappings from term
-variables to their respective type schemes.
-%
-\begin{code}
+
 newtype TypeEnv = TypeEnv (Map.Map String Scheme)
-\end{code}
-%
-We define several functions on type environments.  The operation
-$\Gamma\backslash x$ removes the binding for $x$ from $\Gamma$ and is
-called |remove|.
-%
-\begin{code}
 instance Types TypeEnv where
     ftv (TypeEnv env)      =  ftv (Map.elems env)
     apply s (TypeEnv env)  =  TypeEnv (Map.map (apply s) env)
-\end{code}
-%
-The function |generalize| abstracts a type over all type variables
-which are free in the type but not free in the given type environment.
-%
-\begin{code}
+
 generalize        ::  TypeEnv -> Type -> Scheme
 generalize env t  =   Scheme vars t
   where vars = Set.toList ((ftv t) `Set.difference` (ftv env))
-\end{code}
 
-Several operations, for example type scheme instantiation, require
-fresh names for newly introduced type variables.  This is implemented
-by using an appropriate monad which takes care of generating fresh
-names.  It is also capable of passing a dynamically scoped
-environment, error handling and performing I/O, but we will not go
-into details here.
-\begin{code}
 data TIEnv = TIEnv  {}
 
 data TIState = TIState { tiSupply :: Int }
@@ -256,26 +123,11 @@ newTyVarName prefix =
 
 newTyVar :: Monad m => String -> TI m Type
 newTyVar prefix = TVar <$> newTyVarName prefix
-\end{code}
-%
-The instantiation function replaces all bound type variables in a type
-scheme with fresh type variables.
-%
-\begin{code}
+
 instantiate :: Monad m => Scheme -> TI m Type
 instantiate (Scheme vars t) = do  nvars <- mapM (\ _ -> newTyVar "a") vars
                                   let s = Subst (Map.fromList (zip vars nvars))
                                   return $ apply s t
-\end{code}
-%
-This is the unification function for types.  For two types $t_1$ and
-$t_2$, $mgu(t_1, t_2)$ returns the most general unifier.  A unifier is
-a substitution $S$ such that $S(t_1) \equiv S(t_2)$.  The function |varBind| attempts to bind a type
-variable to a type and return that binding as a subsitution, but
-avoids binding a variable to itself and performs the occurs check,
-which is responsible for circularity type errors.
-%
-\begin{code}
 varBind :: Monad m => String -> Type -> TIW m ()
 varBind u t  | t == TVar u           =  return ()
              | u `Set.member` ftv t  =  throwError $ "occurs check fails: " ++ u ++
@@ -286,6 +138,7 @@ data FlatRecord = FlatRecord
   { _frFields :: Map.Map String Type
   , _frExtension :: Maybe String -- TyVar of more possible fields
   }
+
 frFields :: Lens' FlatRecord (Map.Map String Type)
 frFields f (FlatRecord fields ext) = (`FlatRecord` ext) <$> f fields
 
@@ -364,17 +217,6 @@ mgu t@TRecExtend {}
     u@TRecExtend {}          =  join $ either throwError return $ unifyRecs <$> flattenRec t <*> flattenRec u
 mgu t1 t2                    =  throwError $ "types do not unify: " ++ show t1 ++
                                 " vs. " ++ show t2
-\end{code}
-
-\subsection{Main type inference function}
-%
-The function |ti| infers the types for expressions.  The type
-environment must contain bindings for all free variables of the
-expressions.  The returned substitution records the type constraints
-imposed on type variables by the expression, and the returned type is
-the type of the expression.
-%
-\begin{code}
 typeInference :: Monad m => Map.Map String Scheme -> Exp a -> TI m (Exp (Type, a))
 typeInference rootEnv rootExpr =
     do  ((_, t), s) <- runWriterT $ ti (,) (TypeEnv rootEnv) rootExpr
@@ -430,16 +272,7 @@ ti f env expr@(Exp pl body) = case body of
 tiLit :: Monad m => Lit -> TIW m Type
 tiLit (LInt _)   =  return (TCon "Int")
 tiLit (LChar _)  =  return (TCon "Char")
-\end{code}
 
-\subsection{Tests}
-\label{sec:example-expressions}
-
-The following simple expressions (partly taken from
-\cite{Heeren2002GeneralizingHM}) are provided for testing the type
-inference function.
-%
-\begin{code}
 eLet :: String -> Exp () -> Exp () -> Exp ()
 eLet name e1 e2 = Exp () $ ELet name e1 e2
 
@@ -512,50 +345,16 @@ exp9  =  eLet
                  eRecEmpty ) $
          eGetField (eVar "vec") "z"
 
-\end{code}
-%
-This simple test function tries to infer the type for the given
-expression.  If successful, it prints the expression together with its
-type, otherwise, it prints the error message.
-%
-\begin{code}
 test :: Exp a -> IO ()
 test e =
     do  res <- runTI (typeInference Map.empty e)
         case res of
           Left err               ->  putStrLn $ show e ++ "\n " ++ err ++ "\n"
           Right (Exp (t, _) _)   ->  putStrLn $ show e ++ " :: " ++ show t ++ "\n"
-\end{code}
 
-\subsection{Main Program}
-
-The main program simply infers the types for all the example
-expression given in Section~\ref{sec:example-expressions} and prints
-them together with their inferred types, or prints an error message if
-type inference fails.
-
-\begin{code}
 main :: IO ()
 main = mapM_ test [exp0, exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9]
--- |Collecting Constraints|
--- |main = mapM_ test' [exp0, exp1, exp2, exp3, exp4, exp5]|
-\end{code}
-%
-This completes the implementation of the type inference algorithm.
 
-\bibliographystyle{plain}
-\bibliography{bibliography}
-
-\appendix
-
-\section{Pretty-printing}
-\label{sec:pretty-printing}
-
-This appendix defines pretty-printing functions and instances for
-|Show| for all interesting type definitions.
-
-%
-\begin{code}
 instance Show Type where
     showsPrec _ x = shows (prType x)
 
@@ -654,75 +453,3 @@ prScheme (Scheme vars t)  =   PP.text "All" <+>
                               PP.hcat
                                 (PP.punctuate PP.comma (map PP.text vars))
                               <> PP.text "." <+> prType t
-\end{code}
-
-\end{document}
-
-\begin{code}
--- test' :: Exp -> IO ()
--- test' e =
---     do res <- runTI (bu Set.empty e)
---        case res of
---          Left err -> putStrLn $ "error: " ++ err
---          Right t  -> putStrLn $ show e ++ " :: " ++ show t
-
-data Constraint = CEquivalent Type Type
-                | CExplicitInstance Type Scheme
-                | CImplicitInstance Type (Set.Set String) Type
-
-instance Show Constraint where
-    showsPrec _ x = shows (prConstraint x)
-
-prConstraint :: Constraint -> PP.Doc
-prConstraint (CEquivalent t1 t2) = PP.hsep [prType t1, PP.text "=", prType t2]
-prConstraint (CExplicitInstance t s) =
-    PP.hsep [prType t, PP.text "<~", prScheme s]
-prConstraint (CImplicitInstance t1 m t2) =
-    PP.hsep [prType t1,
-             PP.text "<=" <>
-               PP.parens (PP.hcat (PP.punctuate PP.comma (map PP.text (Set.toList m)))),
-             prType t2]
-
-type Assum = [(String, Type)]
-type CSet = [Constraint]
-
--- bu :: Monad m => Set.Set String -> Exp -> TI m (Assum, CSet, Type)
--- bu _m (EVar n) = do b <- newTyVar "b"
---                     return ([(n, b)], [], b)
--- bu _m (ELit (LInt _)) = do b <- newTyVar "b"
---                            return ([], [CEquivalent b (TCon "Int")], b)
--- bu _m (ELit (LChar _)) = do b <- newTyVar "b"
---                             return ([], [CEquivalent b (TCon "Char")], b)
--- bu m (EApp e1 e2) =
---     do (a1, c1, t1) <- bu m e1
---        (a2, c2, t2) <- bu m e2
---        b <- newTyVar "b"
---        return (a1 ++ a2, c1 ++ c2 ++ [CEquivalent t1 (TFun t2 b)],
---                b)
--- bu m (EAbs x body) =
---     do b@(TVar vn) <- newTyVar "b"
---        (a, c, t) <- bu (vn `Set.insert` m) body
---        return (a `removeAssum` x, c ++ [CEquivalent t' b | (x', t') <- a,
---                                         x == x'], TFun b t)
--- bu m (ELet x e1 e2) =
---     do (a1, c1, t1) <- bu m e1
---        (a2, c2, t2) <- bu (x `Set.delete` m) e2
---        return (a1 ++ removeAssum a2 x,
---                c1 ++ c2 ++ [CImplicitInstance t' m t1 |
---                             (x', t') <- a2, x' == x], t2)
-
--- removeAssum :: Eq a => [(a, t)] -> a -> [(a, t)]
--- removeAssum [] _ = []
--- removeAssum ((n', _) : as) n | n == n' = removeAssum as n
--- removeAssum (a:as) n = a : removeAssum as n
-\end{code}
-
-\bibliographystyle{plain}
-\bibliography{bibliography}
-
-\end{document}
-
-% Local Variables:
-% mode: latex
-% mmm-classes: literate-haskell-latex
-% End:

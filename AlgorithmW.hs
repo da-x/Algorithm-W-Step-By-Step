04d587cc4ebc2ff93573b19003278913a7ec5611
Eyal Lotem <eyal.lotem@elastifile.com>
2014-06-23 00:39:25 +0300

    Split AlgorithmW.hs -> Test.hs

--- AlgorithmW.hs
+++ AlgorithmW.hs
@@ -1,5 +1,14 @@
 {-# LANGUAGE StandaloneDeriving, DeriveFunctor, DeriveFoldable, DeriveTraversable, DeriveGeneric #-}
-module AlgorithmW where
+module AlgorithmW
+  ( Lit(..)
+  , Leaf(..)
+  , Body(..)
+  , Exp(..), expPayload
+  , Scheme(..)
+  , Type(..)
+  , eLet, eAbs, eVar, eLit, eRecEmpty, eApp, eRecExtend, eGetField
+  , typeInference
+  ) where
 
 import Control.Applicative
 import Control.DeepSeq
@@ -304,63 +313,6 @@ eRecExtend name typ rest = Exp () $ ERecExtend name typ rest
 
 eGetField :: Exp () -> String -> Exp ()
 eGetField r n = Exp () $ EGetField r n
-
-exp0 :: Exp ()
-exp0  =  eLet "id" (eAbs "x" (eVar "x"))
-          (eVar "id")
-
-exp1 :: Exp ()
-exp1  =  eLet "id" (eAbs "x" (eVar "x"))
-          (eApp (eVar "id") (eVar "id"))
-
-exp2 :: Exp ()
-exp2  =  eLet "id" (eAbs "x" (eLet "y" (eVar "x") (eVar "y")))
-          (eApp (eVar "id") (eVar "id"))
-
-exp3 :: Exp ()
-exp3  =  eLet "id" (eAbs "x" (eLet "y" (eVar "x") (eVar "y")))
-          (eApp (eApp (eVar "id") (eVar "id")) (eLit (LInt 2)))
-
-exp4 :: Exp ()
-exp4  =  eLet "id" (eAbs "x" (eApp (eVar "x") (eVar "x")))
-          (eVar "id")
-
-exp5 :: Exp ()
-exp5  =  eAbs "m" (eLet "y" (eVar "m")
-                   (eLet "x" (eApp (eVar "y") (eLit (LChar 'x')))
-                         (eVar "x")))
-
-exp6 :: Exp ()
-exp6  =  eApp (eLit (LInt 2)) (eLit (LInt 2))
-
-exp7 :: Exp ()
-exp7  =  eAbs "vec" $
-         eRecExtend "newX" (eGetField (eVar "vec") "x") $
-         eRecExtend "newY" (eGetField (eVar "vec") "y") $
-         eRecEmpty
-
-exp8 :: Exp ()
-exp8  =  eLet
-         "vec" ( eRecExtend "x" (eLit (LInt 5)) $
-                 eRecExtend "y" (eLit (LInt 7)) $
-                 eRecEmpty ) $
-         eGetField (eVar "vec") "x"
-
-exp9 :: Exp ()
-exp9  =  eLet
-         "vec" ( eRecExtend "x" (eLit (LInt 5)) $
-                 eRecExtend "y" (eLit (LInt 7)) $
-                 eRecEmpty ) $
-         eGetField (eVar "vec") "z"
-
-test :: Exp a -> IO ()
-test e =
-    case typeInference Map.empty e of
-        Left err               ->  putStrLn $ show e ++ "\n " ++ err ++ "\n"
-        Right (Exp (t, _) _)   ->  putStrLn $ show e ++ " :: " ++ show t ++ "\n"
-
-main :: IO ()
-main = mapM_ test [exp0, exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9]
 
 instance Show Type where
     showsPrec _ x = shows (prType x)

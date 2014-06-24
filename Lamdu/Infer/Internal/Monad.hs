ca7283f966d1d31330f5fc1b992a166de87da9bb
Eyal Lotem <eyal.lotem@elastifile.com>
2014-06-24 19:38:54 +0300

    Infer.Internal.Monad: Qualified imports

--- Lamdu/Infer/Internal/Monad.hs
+++ Lamdu/Infer/Internal/Monad.hs
@@ -11,20 +11,20 @@ import Control.Monad.State (evalState, State)
 import Control.Monad.Trans.Either (EitherT, runEitherT)
 import Control.Monad.Writer (WriterT, runWriterT)
 import Lamdu.Expr
-import Lamdu.Infer.Internal.FreeTypeVars
+import qualified Lamdu.Infer.Internal.FreeTypeVars as FreeTypeVars
 import qualified Control.Monad.State as State
 
 data InferState = InferState { inferSupply :: Int }
 
 -- TODO: Consider newtypes?
 type Infer = EitherT String (State InferState)
-type InferW = WriterT Subst Infer
+type InferW = WriterT FreeTypeVars.Subst Infer
 
 run :: Infer a -> Either String a
 run t = evalState (runEitherT t) initInferState
   where initInferState = InferState{inferSupply = 0}
 
-runW :: InferW a -> Infer (a, Subst)
+runW :: InferW a -> Infer (a, FreeTypeVars.Subst)
 runW = runWriterT
 
 newTyVarName :: String -> Infer TypeVar

ddc5831265d92438b6bae643416ed198a4edd759
Eyal Lotem <eyal.lotem@gmail.com>
2014-07-22 20:29:20 +0300

    TypeVars: Reorganize order of class declarations

--- Lamdu/Infer/Internal/TypeVars.hs
+++ Lamdu/Infer/Internal/TypeVars.hs
@@ -43,6 +43,15 @@ instance Monoid Subst where
     (t1 `Map.union` Map.map (applySubst s1) t0)
     (r1 `Map.union` Map.map (applySubst s1) r0)
 
+class FreeTypeVars a where
+  freeTypeVars :: a -> TypeVars
+  applySubst   :: Subst -> a -> a
+
+class CompositeHasVar p where
+  compositeGetVars :: TypeVars -> Set (E.TypeVar (E.CompositeType p))
+  compositeNewVars :: Set (E.TypeVar (E.CompositeType p)) -> TypeVars
+  compositeGetSubst :: Subst -> SubSubst (E.CompositeType p)
+  compositeNewSubst :: SubSubst (E.CompositeType p) -> Subst
 
 class FreeTypeVars t => HasVar t where
   getVars :: TypeVars -> Set (E.TypeVar t)
@@ -50,18 +59,32 @@ class FreeTypeVars t => HasVar t where
   liftVar :: E.TypeVar t -> t
   newSubst :: E.TypeVar t -> t -> Subst
 
+instance CompositeHasVar p => FreeTypeVars (E.CompositeType p) where
+  freeTypeVars E.CEmpty          = mempty
+  freeTypeVars (E.CVar n)        = newVars (Set.singleton n)
+  freeTypeVars (E.CExtend _ t r) = freeTypeVars t `mappend` freeTypeVars r
+
+  applySubst _ E.CEmpty          = E.CEmpty
+  applySubst s (E.CVar n)        = fromMaybe (E.CVar n) $ Map.lookup n (compositeGetSubst s)
+  applySubst s (E.CExtend n t r) = E.CExtend n (applySubst s t) (applySubst s r)
+
+instance FreeTypeVars E.Type where
+  freeTypeVars (E.TVar n)      =  newVars (Set.singleton n)
+  freeTypeVars (E.TInst _ p)   =  mconcat $ map freeTypeVars $ Map.elems p
+  freeTypeVars (E.TFun t1 t2)  =  freeTypeVars t1 `mappend` freeTypeVars t2
+  freeTypeVars (E.TRecord r)   =  freeTypeVars r
+
+  applySubst s (E.TVar n)      = fromMaybe (E.TVar n) $ Map.lookup n (substTypes s)
+  applySubst s (E.TInst n p)   = E.TInst n $ applySubst s <$> p
+  applySubst s (E.TFun t1 t2)  = E.TFun (applySubst s t1) (applySubst s t2)
+  applySubst s (E.TRecord r)   = E.TRecord $ applySubst s r
+
 instance HasVar E.Type where
   getVars (TypeVars vs _) = vs
   newVars vs = TypeVars vs mempty
   liftVar = E.TVar
   newSubst tv t = Subst (Map.singleton tv t) mempty
   {-# INLINE newSubst #-}
-
-class CompositeHasVar p where
-  compositeGetVars :: TypeVars -> Set (E.TypeVar (E.CompositeType p))
-  compositeNewVars :: Set (E.TypeVar (E.CompositeType p)) -> TypeVars
-  compositeGetSubst :: Subst -> SubSubst (E.CompositeType p)
-  compositeNewSubst :: SubSubst (E.CompositeType p) -> Subst
 
 instance CompositeHasVar E.Product where
   compositeGetVars (TypeVars _ vs) = vs
@@ -84,27 +107,3 @@ difference (TypeVars t0 r0) (TypeVars t1 r1) =
 substDeleteVars :: TypeVars -> Subst -> Subst
 substDeleteVars (TypeVars t r) (Subst st sr) =
   Subst (MapU.deleteKeySet t st) (MapU.deleteKeySet r sr)
-
-class FreeTypeVars a where
-  freeTypeVars :: a -> TypeVars
-  applySubst   :: Subst -> a -> a
-
-instance CompositeHasVar p => FreeTypeVars (E.CompositeType p) where
-  freeTypeVars E.CEmpty          = mempty
-  freeTypeVars (E.CVar n)        = newVars (Set.singleton n)
-  freeTypeVars (E.CExtend _ t r) = freeTypeVars t `mappend` freeTypeVars r
-
-  applySubst _ E.CEmpty          = E.CEmpty
-  applySubst s (E.CVar n)        = fromMaybe (E.CVar n) $ Map.lookup n (compositeGetSubst s)
-  applySubst s (E.CExtend n t r) = E.CExtend n (applySubst s t) (applySubst s r)
-
-instance FreeTypeVars E.Type where
-  freeTypeVars (E.TVar n)      =  newVars (Set.singleton n)
-  freeTypeVars (E.TInst _ p)   =  mconcat $ map freeTypeVars $ Map.elems p
-  freeTypeVars (E.TFun t1 t2)  =  freeTypeVars t1 `mappend` freeTypeVars t2
-  freeTypeVars (E.TRecord r)   =  freeTypeVars r
-
-  applySubst s (E.TVar n)      = fromMaybe (E.TVar n) $ Map.lookup n (substTypes s)
-  applySubst s (E.TInst n p)   = E.TInst n $ applySubst s <$> p
-  applySubst s (E.TFun t1 t2)  = E.TFun (applySubst s t1) (applySubst s t2)
-  applySubst s (E.TRecord r)   = E.TRecord $ applySubst s r

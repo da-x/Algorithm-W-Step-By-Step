685d05f6a2accab2a49992686b16f2248c48e93b
Eyal Lotem <eyal.lotem@gmail.com>
2014-08-13 01:33:32 +0300

    Add leaf/hole pure expr combinators

--- Lamdu/Expr/Pure.hs
+++ Lamdu/Expr/Pure.hs
@@ -1,5 +1,5 @@
 module Lamdu.Expr.Pure
-  ( abs, var, global, litInt, recEmpty, app, recExtend, getField
+  ( abs, var, global, litInt, recEmpty, app, recExtend, getField, leaf, hole
   ) where
 
 import Prelude hiding (abs)
@@ -8,14 +8,17 @@ import qualified Lamdu.Expr as E
 abs :: E.ValVar -> E.Val () -> E.Val ()
 abs name body = E.Val () $ E.VAbs $ E.Lam name body
 
+leaf :: E.ValLeaf -> E.Val ()
+leaf = E.Val () . E.VLeaf
+
 var :: E.ValVar -> E.Val ()
-var = E.Val () . E.VLeaf . E.VVar
+var = leaf . E.VVar
 
 global :: E.GlobalId -> E.Val ()
-global = E.Val () . E.VLeaf . E.VGlobal
+global = leaf . E.VGlobal
 
 litInt :: Integer -> E.Val ()
-litInt = E.Val () . E.VLeaf . E.VLiteralInteger
+litInt = leaf . E.VLiteralInteger
 
 recEmpty :: E.Val ()
 recEmpty = E.Val () $ E.VLeaf E.VRecEmpty
@@ -28,3 +31,6 @@ recExtend name typ rest = E.Val () $ E.VRecExtend $ E.RecExtend name typ rest
 
 getField :: E.Val () -> E.Tag -> E.Val ()
 getField r n = E.Val () $ E.VGetField $ E.GetField r n
+
+hole :: E.Val ()
+hole = leaf E.VHole

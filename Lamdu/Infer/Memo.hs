d77dad007ddbe2d23fa88a4fba4ed575f07f961a
Eyal Lotem <eyal.lotem@gmail.com>
2014-12-06 23:57:19 +0200

    Better Memo without Ord instance needed

--- Lamdu/Infer/Memo.hs
+++ Lamdu/Infer/Memo.hs
@@ -4,20 +4,25 @@ module Lamdu.Infer.Memo
   , loadInfer
   ) where
 
-import Control.Monad.Trans.State (StateT(..))
+import Control.Lens.Operators
+import Control.Monad.Trans.State (StateT(..), evalState, state)
+import Data.Functor (void)
 import Data.Map (Map)
 import Data.MemoUgly (memo)
+import Data.Traversable (Traversable, traverse)
 import Lamdu.Expr.Scheme (Scheme)
 import Lamdu.Expr.Val (Val)
 import Lamdu.Infer (Scope, Infer(..), Payload)
+import qualified Data.Foldable as Foldable
 import qualified Lamdu.Expr.Val as V
 import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Infer.Load as InferLoad
 
-newtype Memo a = Memo
-  { infer :: Map V.GlobalId Scheme -> Scope -> Val a -> Infer (Val (Payload, a)) }
 
-make :: Ord a => Memo a
+newtype Memo = Memo
+  { infer :: Map V.GlobalId Scheme -> Scope -> Val () -> Infer (Val Payload) }
+
+make :: Memo
 make = Memo $
   memo $ \globals ->
   memo $ \scope ->
@@ -25,10 +30,22 @@ make = Memo $
   Infer $ StateT $
   memo $
   runStateT $ Infer.run $
-  Infer.infer globals scope val
+  Infer.infer globals scope val <&> fmap fst
+
+zipTraversable :: Traversable t => t a -> t b -> t (a, b)
+zipTraversable a b =
+  -- TODO: Assert resulting state is []
+  evalState (traverse (const next) a) pairs
+  where
+    -- TODO: Better assertion about end of list
+    next = state (\(x:xs) -> (x, xs))
+    -- TODO: zip that verifies same length
+    pairs = zip (Foldable.toList a) (Foldable.toList b)
 
 loadInfer ::
-  (Ord a, Monad m) => InferLoad.Loader m -> Memo a -> Scope -> Val a -> m (Infer (Val (Payload, a)))
+  Monad m => InferLoad.Loader m -> Memo -> Scope -> Val a -> m (Infer (Val (Payload, a)))
 loadInfer loader iMemo scope val =
   do  globals <- InferLoad.loadValGlobals loader val
-      return $ infer iMemo globals scope val
+      infer iMemo globals scope (void val)
+        <&> (`zipTraversable` val)
+        & return

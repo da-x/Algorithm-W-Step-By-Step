53ab2b2222eba1803a7278341d203a655c10f4a0
Eyal Lotem <eyal.lotem@gmail.com>
2014-12-24 21:41:51 +0200

    Update module to expose CanSubst, reduce API size, allow subst on scope

--- Lamdu/Infer/Update.hs
+++ Lamdu/Infer/Update.hs
@@ -1,10 +1,9 @@
 module Lamdu.Infer.Update
-  ( update, updatePayload, updateInferredVal
+  ( Subst.CanSubst, update, updateInferredVal
   ) where
 
 import Control.Lens.Tuple
 import Data.Traversable (traverse)
-import Lamdu.Expr.Type (Type)
 import Lamdu.Expr.Val (Val)
 import Lamdu.Infer (Infer)
 import qualified Lamdu.Infer as Infer
@@ -16,15 +15,10 @@ import qualified Lamdu.Infer.Internal.Subst as Subst
 -- variables that have known substitutions. To update old types to
 -- contain knowledge from all accumulated substitutions, use this
 -- action.
-update :: Type -> Infer Type
+update :: Subst.CanSubst a => a -> Infer a
 update typ =
   do  s <- M.getSubst
       return $ Subst.apply s typ
 
-updatePayload :: Infer.Payload -> Infer Infer.Payload
-updatePayload (Infer.Payload typ scope) =
-  do  s <- M.getSubst
-      return $ Infer.Payload (Subst.apply s typ) (Subst.apply s scope)
-
 updateInferredVal :: Val (Infer.Payload, a) -> Infer (Val (Infer.Payload, a))
-updateInferredVal = (traverse . _1) updatePayload
+updateInferredVal = (traverse . _1) update

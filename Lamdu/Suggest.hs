5bfaa5cedbab286d64ecd7360cc7fde8051a2ba5
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-24 00:49:54 +0300

    Add suggest of case analysis

--- Lamdu/Suggest.hs
+++ Lamdu/Suggest.hs
@@ -14,7 +14,10 @@ suggestValueWith _ T.TVar{}                  = pure P.hole
 suggestValueWith _ T.TInst{}                 = pure P.hole
 -- TODO: Special case sum of 1?
 suggestValueWith _ T.TSum {}                 = pure P.hole
-suggestValueWith mkVar (T.TRecord composite) = suggestRecordWith mkVar composite
+suggestValueWith mkVar (T.TRecord composite) =
+    suggestRecordWith mkVar composite
+suggestValueWith mkVar (T.TFun (T.TSum composite) r) =
+    suggestCaseWith mkVar composite r
 suggestValueWith mkVar (T.TFun _ r)          =
     P.abs <$> mkVar <*> suggestValueWith mkVar r
 
@@ -23,3 +26,11 @@ suggestRecordWith _ T.CVar{}          = pure P.hole
 suggestRecordWith _ T.CEmpty          = pure P.recEmpty
 suggestRecordWith mkVar (T.CExtend f t r) =
     P.recExtend f <$> suggestValueWith mkVar t <*> suggestRecordWith mkVar r
+
+suggestCaseWith :: Applicative f => f V.Var -> T.Composite T.Sum -> Type -> f (Val ())
+suggestCaseWith _ T.CVar{} _ = pure P.hole
+suggestCaseWith _ T.CEmpty _ = pure P.absurd
+suggestCaseWith mkVar (T.CExtend f t r) res =
+    P._case f
+    <$> suggestValueWith mkVar (T.TFun t res)
+    <*> suggestCaseWith mkVar r res
